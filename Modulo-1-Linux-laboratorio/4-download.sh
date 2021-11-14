5e6ea4950bad62545aa2c56097c8e485c7ee8b2e
Ricard Forcada <ricard.forcada@roche.com>
2021-11-14 13:06:22 +0100

    Añadido control de parametros ejecricio 1

--- Modulo-1-Linux-laboratorio/4-download.sh
+++ Modulo-1-Linux-laboratorio/4-download.sh
@@ -1,5 +1,5 @@
 if [ "$#" -ne 2 ]; then
-    echo "Se requieren 2 parametro para la ejecucción del script (palabra a buscar y web), ej: sh ./4-download.sh google https://www.google.com"
-    exit 1
+  echo "Se requieren 2 parametro para la ejecucción del script (palabra a buscar y web), ej: sh ./4-download.sh google https://www.google.com"
+  exit 1
 fi
 wget -qO- $2 | grep -na $1 | head -n 1 | awk -F ':' '{print $1}'
